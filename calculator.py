2599a133adfd279cacce0a422c1914c36468ce1a
Santhosh09062006 <sandy09062006@gmail.com>
2026-09-14 15:01:07 +0530

    Fix calculator for automated testing

--- calculator.py
+++ calculator.py
@@ -1,20 +1,29 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     return a / b
 
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+def square(a):
+    return a * a
 
-print("Addition:", add(a, b))
-print("Subtraction:", subtract(a, b))
-print("Multiplication:", multiply(a, b))
-print("Division:", divide(a, b))
+
+if __name__ == "__main__":
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiply(a, b))
+    print("Division:", divide(a, b))
+    print("Square of first number:", square(a))
